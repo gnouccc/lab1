f9c5e1e15ae6832c157c534342a073b2d536344f
gnouccc <100700646+gnouccc@users.noreply.github.com>
2022-03-02 19:53:15 +0700

    Update server.js

--- server.js
+++ server.js
@@ -6,3 +6,10 @@ const POST = config.app.port:
 app.listen(PORT, () => {
     console.log('Server is running on port ${PORT}.');
 });
+const config ={
+    app:{
+        port: process.env.Port || 8080,
+    },
+};
+
+module.exports = config;
