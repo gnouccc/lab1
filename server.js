caf3c00d2223cd2a5b495d08e7e25ae57016ebf2
gnouccc <100700646+gnouccc@users.noreply.github.com>
2022-03-02 19:50:28 +0700

    server.js

---
+++ server.js
@@ -0,0 +1,8 @@
+const app = require("./app");
+const congfig = require("./app/config");
+
+// start server
+const POST = config.app.port:
+app.listen(PORT, () => {
+    console.log('Server is running on port ${PORT}.');
+});
